e976092c6dda9d2fd9159911bb9ffec64f7837a2
JoaoPauluu <jpgasperi1@gmail.com>
2021-08-28 20:36:42 -0300

    On and Off commands working with mongodb

--- commands/off.js
+++ commands/off.js
@@ -1,3 +1,41 @@
+const mongoUtil = require('../scripts/mongoUtil.js');
+
+
+module.exports = {
+	name: 'off',
+	description: 'Disable the feature that makes the bot follow users in voice channels',
+	async execute(message, args) {
+		const query = { id: `${message.guild.id}`};
+		const collection = await mongoUtil.getCol('servers');
+		const searchResult = await collection.findOne(query);
+		if(!searchResult) {
+			await collection.insertOne({ id: `${message.guild.id}`, chase: false});
+			message.guild.me.setNickname('Maxuga [OFF]');
+			message.channel.send('Agora NÃO ESTOU seguindo os outros nas chamadas');
+		} else
+		if(searchResult.chase == true) {
+			await collection.updateOne(query, {
+				$set: {
+					chase: false
+				}
+			})
+			message.guild.me.setNickname('Maxuga [OFF]');
+			message.channel.send('Agora NÃO ESTOU seguindo os outros nas chamadas');
+		} else
+		if(searchResult.chase == false) {
+			message.channel.send('Já estou desabilitado');
+		} else {
+			message.channel.send('Algo deu errado');
+			console.log(searchResult);
+		}
+	}
+};
+
+
+
+
+
+/*
 module.exports = {
 	name: 'off',
 	description: 'Disable the feature that makes the bot follow users in voice channels',
@@ -7,3 +45,4 @@ module.exports = {
         joinCall = false;
 	},
 };
+*/
--- commands/on.js
+++ commands/on.js
@@ -1,3 +1,39 @@
+const mongoUtil = require('../scripts/mongoUtil.js');
+
+
+module.exports = {
+	name: 'on',
+	description: 'Disable the feature that makes the bot follow users in voice channels',
+	async execute(message, args) {
+		const query = { id: `${message.guild.id}`};
+		const collection = await mongoUtil.getCol('servers');
+		const searchResult = await collection.findOne(query);
+		if(!searchResult) {
+			await collection.insertOne({ id: `${message.guild.id}`, chase: false});
+			message.guild.me.setNickname('Maxuga [ON]');
+			message.channel.send('Agora ESTOU seguindo os outros nas chamadas');
+		} else
+		if(searchResult.chase == false) {
+			await collection.updateOne(query, {
+				$set: {
+					chase: true
+				}
+			})
+			message.guild.me.setNickname('Maxuga [ON]');
+			message.channel.send('Agora ESTOU seguindo os outros nas chamadas');
+		} else
+		if(searchResult.chase == true) {
+			message.channel.send('Já estou habilitado');
+		} else {
+			console.log(searchResult);
+			message.channel.send('Algo deu errado');
+		}
+	},
+};
+
+
+
+/*
 module.exports = {
 	name: 'on',
 	description: 'Enable the feature that makes the bot follow users in voice channels',
@@ -7,3 +43,4 @@ module.exports = {
         joinCall = true;
 	},
 };
+*/
