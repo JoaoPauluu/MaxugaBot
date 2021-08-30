de2011127638c474f6f32548af019a8bb04af0eb
JoaoPauluu <jpgasperi1@gmail.com>
2021-08-30 12:34:46 -0300

    maxuga eval now deletes the invoking message

--- commands/eval.js
+++ commands/eval.js
@@ -1,15 +1,18 @@
+const embeds = require('../scripts/embed.js');
+
 module.exports = {
 	name: 'eval',
 	description: 'Runs a string given by the user (bot owner only)',
 	execute(message, args) {
 		if(message.author.id !== '758328146377834549') {
-			message.channel.send('Esse comando só funciona para o dono do bot');
+			message.channel.send(embeds.simpleEmbed(`<@${message.author.id}> Esse comando só funciona para o dono do bot`));
 			return;
 		} else {
 			fullArg = '';
 			args.forEach(singleArg => {
 				fullArg = fullArg + ' ' + singleArg;
 			})
+			message.delete();
 			eval(fullArg);
 		}
 	},
